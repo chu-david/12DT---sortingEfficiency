48f3a136bd5128d84c50b18d5bdacf703b6bb998
chu-david <dchu@emmauscc.sa.edu.au>
2019-03-24 15:14:04 +1030

    initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    //main function
+
+    ReadFile();
+
+    numToSort = randArray(10,5,10);
+    console.log(numToSort);
+
+    /*
+    var sortByBubble = bubbleSort(numToSort);
+
+    var sortByInsertion = insertionSort(numToSort);
+    */
+
+})
+
+function randInt(lowerBound,upperBound){
+    var intNum = lowerBound+ Math.floor(Math.random()*(upperBound-lowerBound+1));
+return intNum
+}
+
+function randArray(size, lowerBound, upperBound){
+
+    IntArray = [];
+
+    for(var i=0;i<size;i++){
+
+        IntArray.push(randInt(lowerBound,upperBound));
+
+    }
+return IntArray;
+}
+
+function bubbleSort(toSort){
+    console.log("running bubble sort");
+}
+
+function insertionSort(toSort){
+
+    console.log("runing insertion sort");
+
+}
+
+function ReadFile(){
+	
+    $.ajax({
+        
+        url: "media/countriesOfTheWorld.csv", dataType:"text", async: false, 
+        success: function(result){
+            
+
+        
+        }
+    });
+
+}
