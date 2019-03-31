3150987c91b334267b3169fa0a3449d46f66b219
chu-david <dchu@emmauscc.sa.edu.au>
2019-03-31 16:04:14 +1030

    insertion sort and lastnumber solution and simulation included

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 $(document).ready(function(){
     //main function
 
-    ReadFile();
+    //ReadFile();
 
-    numToSort = randArray(10,5,10);
-    console.log(numToSort);
+    numToSort = randArray(10,0,10);
+    //console.log(numToSort);
+    var tally= [0,0,0,0,0,0,0,0,0,0,0];
+
+    var simNum = 100000;
+    for(var i=0;i<simNum;i++){
+
+        var testVal = theLastNumber();
+
+        tally[testVal] = tally[testVal]+1;
+    }
+
+    console.log(tally);
 
     /*
     var sortByBubble = bubbleSort(numToSort);
-
+ 
     var sortByInsertion = insertionSort(numToSort);
-    */
+ */  
 
 })
 
@@ -38,7 +49,56 @@ function bubbleSort(toSort){
 function insertionSort(toSort){
 
     console.log("running insertion sort");
+
+    for(var i=1;i<toSort.length;i++){ //loop through the 1st element to last, missing 0th element//
+        console.log("pass"+i);
+        for(var j=i;j>=0;j--){
+
+            if(toSort[j-1]>toSort[j]){
+
+                var temp1 = toSort[j-1];
+                toSort[j-1] = toSort[j];
+                toSort[j] = temp1;
+
+                console.log(toSort);
+            }else{
+
+                break;
+            }
+        }
+
+
+    }
+    console.log(toSort);
+return toSort}
+
+function theLastNumber(){
+    var arrayIn =[];
+    for(var i=1;i<=10;i++){
+        arrayIn.push(i);
+    }
+    
+    while(arrayIn.length>1){
+
+        var index1 = Math.floor(Math.random()*arrayIn.length); //0->9
+        var index2 = Math.floor(Math.random()*arrayIn.length); //0->9
+
+        //console.log(index1);
+        //console.log(index2);
+        while(index2==index1){
+            index2 = Math.floor(Math.random()*arrayIn.length);
+        }
+            arrayIn.push(Math.abs(arrayIn[index1]-arrayIn[index2]));
+
+            arrayIn.splice(index1,1);
+            arrayIn.splice(index2,1);
+
+
+    }
+    //console.log(arrayIn[0]);
+    return arrayIn[0];
 }
+
 
 function ReadFile(){
 	
